3df757e75b56aa4f3c95715fd1cac8401813a199
jasonMisaq <powergame_coder2@yahoo.com>
2017-10-12 01:19:25 +0330

    Added a dismiss service

---
+++ app/src/main/java/com/mehdi/memo/DismissService.java
@@ -0,0 +1,24 @@
+package com.mehdi.memo;
+
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by johndoe on 10/12/17.
+ */
+
+public class DismissService extends IntentService {
+
+    public DismissService() {
+        super("DismissService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent){
+        int notifId=intent.getIntExtra(getString(R.string.notify_id), 0);
+        ((NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE)).cancel(notifId);
+    }
+}
