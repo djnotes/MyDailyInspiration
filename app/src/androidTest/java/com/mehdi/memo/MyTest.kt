8655d75597e14d403a9bfe76a197210cb7300a94
Mehdi Haghgoo <powergame_coder2@yahoo.com>
2022-05-21 06:39:31 -0800

    Added more tests

--- app/src/androidTest/java/com/mehdi/memo/MyTest.kt
+++ app/src/androidTest/java/com/mehdi/memo/MyTest.kt
@@ -1,8 +1,12 @@
 package com.mehdi.memo
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ActivityScenario.ActivityAction
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -28,4 +32,30 @@ class MyTest {
 
         activityScenario.close()
     }
+
+    @Test
+    fun note_isSuccessfullySaved(){
+        val activity = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.fab))
+            .perform(ViewActions.click())
+
+        onView(withId(R.id.text_memo_author))
+            .perform(ViewActions.typeText("Test Note"))
+        onView(withId(R.id.text_memo_note))
+            .perform(ViewActions.typeText("Test Note"))
+        onView(withId(R.id.text_memo_author))
+            .perform(ViewActions.typeText("Test Author"))
+
+        onView(withId(R.id.action_save))
+            .perform(ViewActions.click())
+
+        //Check that we are in MainActivity
+        onView(withId(R.id.fab))
+            .check { view, noViewFoundException ->  view.isShown}
+
+        activity.close()
+    }
+
+
 }
